0b306b73fc5abdb524660ee6aa8db0539bfea44e
saro1516 <saro33777@gmail.com>
2025-12-12 07:47:23 +0530

    input output problems,java

---
+++ java.main/day 1 problem 18/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("size of int:"+Integer.BYTES);
+		System.out.println("size of char:"+Character.BYTES);
+		System.out.println("size of float:"+Float.BYTES);
+		System.out.println("size of double:"+Double.BYTES);
+	}
+}
---
+++ java.main/day 1 problem 20/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	     int b=sc.nextInt();
+	      int c=sc.nextInt();
+	       int d=sc.nextInt();
+	        int e=sc.nextInt();
+	    
+	    
+		System.out.println("maths:"+a);
+		System.out.println("tamil:"+b);
+		System.out.println("english:"+c);
+		System.out.println("sceince:"+d);
+		System.out.println("social sceince:"+e);
+	}
+}
